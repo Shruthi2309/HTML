d50c0865d6e0658d1221dd293426f9a5f0aa4379
Shruthi2309 <shruthibhadani2301@gmail.com>
2024-05-20 14:04:54 +0530

    updated

--- jsproject/script.js
+++ jsproject/script.js
@@ -1,34 +1,377 @@
 const data = 
 [
   {
+    Title:"Heeramandi",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/e/ea/Heeramandi_Poster.jpg",
+    Year:2024,
+    Release_Date:"1 May 2024",
+    Duration:"8 Episodes",
+    Actors:["Manisha Koirala, Sonakshi Sinha, Aditi Rao Hydari, Richa Chadha, Sanjeeda Sheikh, Sharmin Segal Mehta, Taha Shah Badussha"],
+    Director:"Sanjay Leela Bhansali",
+    Geners:["History, Drama, Romance"],
+    Languages:"Hindi, Urdu",
+    Box_office_collection:"",
+    Story:"Heeramandi: The Diamond Bazaar is a 2024 Indian period drama television series created and directed by Sanjay Leela Bhansali. The series is about the lives of tawaifs in the red-light district of Heera Mandi in Lahore during the Indian independence movement against the British Raj."
+  },
+  {
+    Title:"Shaitaan",
+    Poster: "https://upload.wikimedia.org/wikipedia/en/f/f0/Shaitaan_2024_film_theatrical_poster.jpeg",
+    Year:2014,
+    Release_Date:"8 March 2024",
+    Duration:"132 minutes",
+    Actors:["Ajay Devgn, R. Madhavan, Jyothika, Janki Bodiwala, Anngad Raaj"],
+    Director:"Vikas Bahl",
+    Geners:["Horrror, Thriller, Drama"],
+    Languages:"Hindi",
+    Box_office_collection:"211.06 crore rupees",
+    Story:"A family outing takes a terrifying turn when an intruder takes over the body of the teenage daughter, putting her at the mercy of increasingly sinister orders."
+  },
+  {
+    Title:"Padmaavat",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/7/73/Padmaavat_poster.jpg",
+    Year:2018,
+    Release_Date:"25 January 2018",
+    Duration:"163 minutes",
+    Actors:["Deepika Padukone, Shahid Kapoor, Ranveer Singh, Aditi Rao Hydari"],
+    Director:"Sanjay Leela Bhansali",
+    Geners:["History, War, Romance, Historical Fiction, Melodrama, Action"],
+    Languages:"Hindi",
+    Box_office_collection:"₹572 crore rupees",
+    Story:"In medieval India, a tyrannical sultan and his army attack a prosperous kingdom to try and capture the beautiful Queen Padmavati. Her husband Maharawal Ratan Singh assembles his valiant forces to defend his land and the honor of his beloved wife."
+  },
+  {
+    Title:"Gangubai Kathiawadi",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/8/89/Gangubai_Kathiawadi_film_poster.jpg",
+    Year:2022,
+    Release_Date:"25 February 2022",
+    Duration:"154 minutes",
+    Actors:["Alia Bhatt, Shantanu Maheshwari, Vijay Raaz, Indira Tiwari, Seema Pahwa, Jim Sarbh, Ajay Devgn"],
+    Director:"Sanjay Leela Bhansali",
+    Geners:["History, Crime, Crime Fiction, Drama"],
+    Languages:"Hindi",
+    Box_office_collection:"₹209.77 crore rupees",
+    Story:"Young Ganga is tricked by her boyfriend with the promise of a film career and persuaded to leave the countryside for Mumbai. In Mumbai her dreams of a film career come crashing down and she is lured to the underworld."
+  },
+  {
+    Title:"Jajantaram Mamantaram",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/2/28/Jajantaram_mamantaram.jpg",
+    Year:2003,
+    Release_Date:"30 May 2003",
+    Duration:"114 minutes",
+    Actors:["	Jaaved Jaffrey, Gulshan Grover"],
+    Director:"Soumitra Ranade",
+    Geners:["Action, Comedy, Fantasy, Children's flim, Adventure"],
+    Languages:"Hindi",
+    Box_office_collection:"5.7 crore",
+    Story:"Aditya finds his way to an island after he is shipwrecked. Here, he realises he is a giant amongst the other inhabitants, who think of him as their saviour from a demon."
+  },
+  {
     Title:"3 IDIOTS",
-    Poster: "https://bcomber.org/wp-content/uploads/2021/10/Screen-Shot-2021-10-26-at-6.48.45-AM-900x499.png",
+    Poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX2vZnL5vB4Ia_RW-Jc2yqwsDuUXAbiWv3XhHF_OqCBw&s",
     Year: 2009,
     Release_Date:"25 December 2009",
     Duration:"171 minutes",
     Actors:"Aamir Khan, Sharman Joshi, R.Madhavan, Kareena Kapoor, Boman Irani, Om Vaidya, Mona Singh",
     Director:"Rajkumar Hirani",
-    Geners: "Comedy, Drama, Adventure, Romance, Melodrama, Friendship, Motivational",
+    Geners: ["Comedy, Drama, Adventure, Romance, Melodrama, Friendship, Motivational"],
     Languages:"Hindi",
     Box_office_collection:"400.61 crore rupees",
+    Story:"In college, Farhan and Raju form a great bond with Rancho due to his positive and refreshing outlook to life. Years later, a bet gives them a chance to look for their long-lost friend whose existence seems rather elusive."
+  },
+  {
+    Title:"Chennai Express",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/1/1b/Chennai_Express.jpg",
+    Year:2013,
+    Release_Date:"",
+    Duration:"141 minutes",
+    Actors:"9 August 2013",
+    Director:"Rohit Shetty",
+    Geners:["Comedy, Action, Romace, Adventure, Melodrama"],
+    Languages:"Hindi",
+    Box_office_collection:"423 crores rupees",
+    Story:"Rahul, a young man, sets out to immerse his late grandfather's ashes at Rameshwaram. However, when he helps Meena, a runaway bride, board a train, he has to face the ire of her criminal family."
+  },
+  {
+    Title:"Bajirao Mastani",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/c/c0/Bajirao_Mastani_poster.jpg",
+    Year:2015,
+    Release_Date:"18 December 2015",
+    Duration:"158 minutes",
+    Actors:["	Ranveer Singh, Deepika Padukone, Priyanka Chopra"],
+    Director:"Sanjay Leela Bhansali",
+    Geners:["History, War, Romance, Action, Musical"],
+    Languages:"Hindi",
+    Box_office_collection:"356.2 crore rupees",
+    Story:"The heroic Peshwa Bajirao, married to Kashibai, falls in love with Mastani, a warrior princess in distress. They struggle to make their love triumph amid opposition from his conservative family."
+  },
+  {
+    Title:"Bhool Bhulaiyaa 2",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/2/23/Bhool_Bhulaiyaa_2_film_poster.jpg",
+    Year:2022,
+    Release_Date:"20 May 2022",
+    Duration:"145 minutes",
+    Actors:["Tabu, Kartik Aaryan, Kiara Advani"],
+    Director:"Anees Bazmee",
+    Geners:["Horror, Comedy, Suspense, Mystery, Thriller"],
+    Languages:"Hindi",
+    Box_office_collection:"266.88 crore rupees",
+    Story:"When strangers Reet and Ruhan cross paths, they find themselves on a journey that leads them to an abandoned mansion and a dreaded spirit who has been trapped for almost two decades."
+  },
+  {
+    Title:"Bhoot Police",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/4/4f/Bhoot_Police_film_poster.jpg",
+    Year:2021,
+    Release_Date:"10 September 2021",
+    Duration:"129 minutes",
+    Actors:["Saif Ali Khan, Arjun Kapoor, Jacqueline Fernandez, Yami Gautam, Javed Jaffrey"],
+    Director:"Pavan Kirpala",
+    Geners:["Horror, Comedy, Adventure"],
+    Languages:"Hindi",
+    Box_office_collection:"40 crores rupees",
+    Story:"Brothers Vibhooti and Chiraunji are assigned a seemingly ordinary case of hunting down demonic spirits in a remote village. However, they soon realise that there is nothing ordinary about this case."
+  },
+  {
+    Title:"Dangal",
+    Poster: "https://upload.wikimedia.org/wikipedia/en/9/99/Dangal_Poster.jpg",
+    Year: 2016,
+    Release_Date:"23 December 2016",
+    Duration:"161 minutes",
+    Actors:["Aamir Khan,Sakshi Tanwar, Fatima Sana Shaikh,Zaira Wasim, Sanya Malhotra, Suhani Bhatnagar, Aparshakti Khurana "],
+    Director:"Nitesh Tiwari",
+    Geners: ["Action, Sports, Drama"],
+    Languages:"Hindi",
+    Box_office_collection:"2,024 crore rupees",
+    Story:"Mahavir Singh Phogat, a former wrestler, decides to fulfil his dream of winning a gold medal for his country by training his daughters for the Commonwealth Games despite the existing social stigmas."
+  },
+  {
+    Title:"Zindagi Na Milegi Dobara",
+    Poster: "https://upload.wikimedia.org/wikipedia/en/1/17/Zindagi_Na_Milegi_Dobara.jpg",
+    Year: 2011 ,
+    Release_Date:"15 July 2011",
+    Duration:"153 minutes",
+    Actors:["	Hrithik Roshan,Abhay Deol,Farhan Akhtar, Katrina Kaif, Kalki Koechlin"],
+    Director:"Zoya Akhtar",
+    Geners:[""],
+    Languages:"Hindi",
+    Box_office_collection:"1.53 Billion rupees",
+    Story:"Three longtime friends embrace the 'carpe diem' spirit during a bachelor retreat in Spain. Somewhere between adventure sports, tomato fights and flamenco, they conquer their fears and unresolved issues."
+  },
+  {
+    Title:"Golmaal 3",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/f/f4/Golmaal_3.jpg",
+    Year:2010,
+    Release_Date:"4 November 2010",
+    Duration:"140 minutes",
+    Actors:["	Mithun Chakraborty, Ajay Devgn, Kareena Kapoor, Arshad Warsi, Tusshar Kapoor, Shreyas Talpade, Kunal Khemu"],
+    Director:"Rohit Shetty",
+    Geners:["Action, Adventure, Drama, Comedy"],
+    Languages:"Hindi",
+    Box_office_collection:" ₹167 crore",
+    Story:"Cupid strikes when Pritam, a single father, comes across his college sweetheart, Geeta, in Goa. However, when their respective children go at war with each other, the two find it difficult to reunite."
+  },
+  {
+    Title:"Chillar Party",
+    Poster: "https://upload.wikimedia.org/wikipedia/en/4/4d/Chillarparty2.jpg",
+    Year: 2011,
+    Release_Date:"8 July 2011",
+    Duration:"127 minutes",
+    Actors:["Shriya Sharma,Sanath Menon, Rohan Grover, Naman Jain, Aarav Khanna, Vishesh Tiwari, Shashank Shende"],
+    Director:"Vikas Bahl",
+    Geners: ["Comedy, Drama, Children's film, Melodrama"],
+    Languages:"Hindi",
+    Box_office_collection:"5.54 crore rupees",
+    Story:"The children of a society make friends with an orphan and his dog Bhidu. But when a scheming politician plans to get rid of stray dogs, they all come up with a plan to save their four-legged friend."
+  },
+  {
+    Title:"Golmaal Again",
+    Poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFF54NKkCopn-t7kZEV9-ASX0ZyL7HY7B_YevAxZFvnA&s",
+    Year:2017,
+    Release_Date:"20 October 2017",
+    Duration:"152 minutes",
+    Actors:["Ajay Devgan, Tabu, Parineeti Chopra, Arshad Warsi, Tusshar Kapoor, Shreyas Talpade, Kunal Khemu, Neil Nitin Mukesh, Johnny Lever"],
+    Director:"Rohit Shetty",
+    Geners: ["Horror, Action, Comedy, Drama, Fantasy"],
+    Languages:"Hindi",
+    Box_office_collection:"310.98 crore rupees",
+    Story:"Five orphan men return to the orphanage they grew up in to attend their mentor's funeral. However, they encounter the ghost of their childhood friend, Khushi, and help her attain salvation."
+  },
+  {
+    Title:"Dil Bechara",
+    Poster: "https://upload.wikimedia.org/wikipedia/en/3/32/Dil_Bechara_film_poster.jpg",
+    Year:2020,
+    Release_Date:"24 July 2020",
+    Duration:"101 minutes",
+    Actors:["Sushant Singh Rajput, Sanjana Sanghi"],
+    Director:"Mukesh Chhabra",
+    Geners:["RRomace, Drama, Comedy, Melodrama"],
+    Languages:"Hindi",
+    Box_office_collection:"25–30 crore rupees",
+    Story:"While struggling to survive, Manny and Kizie, who each have a terminal illness, fall in love with each other. Thereafter, the two try to spend the rest of their days being happy and staying positive."
+  },
+  {
+    Title:"Kedarnath",
+    Poster: "https://upload.wikimedia.org/wikipedia/en/1/19/Kedarnath_film_Poster.jpg",
+    Year:2018,
+    Release_Date:"7 December 2018",
+    Duration:"116 minutes",
+    Actors:["	Sushant Singh Rajput, Sara Ali Khan"],
+    Director:"Abhishek Kapoor",
+    Geners:["Romantic, Disaster, Religous"],
+    Languages:"Hindi",
+    Box_office_collection:"102.77 crore rupees",
+    Story:"Mansoor, a reserved Muslim porter, falls in love with Mukku, the beautiful and rebellious daughter of a Hindu priest. But their love is put to the test amid misunderstandings and nature's wrath."
+  },
+  {
+    Title:"Street Dancer 3D",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/c/c7/Street_Dancer_3D_poster.jpg",
+    Year:2020,
+    Release_Date:"24 January 2020",
+    Duration:"146 minutes",
+    Actors:["	Varun Dhawan, Shraddha Kapoor, Prabhu Deva, Nora Fatehi, Naufal Azmir Khan"],
+    Director:"Remo D'Souza",
+    Geners:["Action, Comedy, Romance, Dance film"],
+    Languages:"Hindi",
+    Box_office_collection:"₹97 crore rupees",
+    Story:"Two rival dance groups, Street Dancer and Rule Breakers, despise each other and participate in a dance battle. Later, they decide to join hands for a greater cause."
+  },
+  {
+    Title:"83",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/6/69/83_film_poster.jpg",
+    Year:2021,
+    Release_Date:"24 December 2021",
+    Duration:"160 minutes",
+    Actors:["Ranveer Singh, Deepika Padukone"],
+    Director:"Kabir Khan",
+    Geners:["History, Drama, Sports"],
+    Languages:"Hindi",
+    Box_office_collection:"193.73 crore rupees",
+    Story:"Led by an ambitious captain, India charges into the 1983 Cricket World Cup final as the dark horse to claim a historic victory against defending champion West Indies."
+  },
+  {
+    Title:"Sultan",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/1/1f/Sultan_film_poster.jpg",
+    Year:2016,
+    Release_Date:"6 July 2016",
+    Duration:"170 minutes",
+    Actors:["Salman Khan, Anushka Sharma, Randeep Hooda, Amit Sadh"],
+    Director:"Ali Abbas Zafar",
+    Geners:["Action, Comedy, Sport, Romance"],
+    Languages:"Hindi",
+    Box_office_collection:"₹623.33 crore rupees",
+    Story:"After the death of his son, Sultan Ali Khan, a middle-aged wrestler, gives up the sport. Years later, he sets out to revive his career as he needs the prize money and wants to regain his lost respect."
+  },
+  {
+    Title:"Gadar 2",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/6/62/Gadar_2_film_poster.jpg",
+    Year:2023,
+    Release_Date:"11 August 2023",
+    Duration:"170 minutes",
+    Actors:["Sunny Deol, Ameesha Patel, Utkarsh Sharma, Gaurav Chopra, Manish Wadhwa, Simrat Kaur"],
+    Director:"Anil Sharma",
+    Geners:["Action, Adventure, Drama, Thriller"],
+    Languages:"Hindi",
+    Box_office_collection:"691.08 crore rupees",
+    Story:"When Tara Singh goes missing during a skirmish and is believed to be captured in Pakistan, his son Jeete sets out to rescue him and enters a labyrinth from which they both must escape at all costs."
+  },
+  {
+    Title:"Shershaah",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/9/91/Shershaah_film_poster.jpg",
+    Year:2021,
+    Release_Date:"12 August 2021",
+    Duration:"135 minutes",
+    Actors:["Sidharth Malhotra, Kiara Advani"],
+    Director:"Vishnuvardhan",
+    Geners:["War, Action, Drama, Thriller"],
+    Languages:"Hindi",
+    Box_office_collection:"",
+    Story:"Vikram Batra, a young man, dreams of becoming a soldier and falls in love with Dimple. Soon after his training, he climbs the military ranks and contributes to India's victory in the Kargil war."
+  },
+  {
+    Title:"Hichki",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/3/3d/Hichki_poster.jpg",
+    Year:2018,
+    Release_Date:"23 March 2018",
+    Duration:"116 minutes",
+    Actors:["	Rani Mukerji"],
+    Director:"Siddharth P. Malhotra",
+    Geners:["Comedy, Drama, Motivation"],
+    Languages:"Hindi",
+    Box_office_collection:"215 crore rupees",
+    Story:"After several interviews and numerous rejections, a woman who has Tourette's syndrome lands a job as a teacher at an elite school. She soon tries to do whatever she can to make her rebellious and trouble making students realise their full potential."
+  },
+  {
+    Title:"M.S. Dhoni: The Untold Story",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/3/33/M.S._Dhoni_-_The_Untold_Story_poster.jpg",
+    Year:2016,
+    Release_Date:"30 September 2016",
+    Duration:"190 minutes",
+    Actors:["Sushant Singh Rajput, Disha Patani, Kiara Advani, Anupam Kher, Bhumika Chawla, Kranti Prakash Jha, Alok Pandey"],
+    Director:"Neeraj Pandey",
+    Geners:["Drama, Sport, Biograph"],
+    Languages:"Hindi",
+    Box_office_collection:"₹216 crore rupees",
+    Story:"M S Dhoni, a boy from Ranchi, aspires to play cricket for India. Though he initially tries to please his father by working for the Indian Railways, he ultimately decides to chase his dreams."
+  },
+  {
+    Title:"Animal",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/9/90/Animal_%282023_film%29_poster.jpg",
+    Year:2023,
+    Release_Date:"1 December 2023",
+    Duration:"201 minutes",
+    Actors:["Ranbir Kapoor, Anil Kapoor, Bobby Deol, Rashmika Mandanna, Triptii Dimri"],
+    Director:"Sandeep Reddy Vanga",
+    Geners:["Action, Adeventure, Crime, Thriller"],
+    Languages:"Hindi",
+    Box_office_collection:"917.82 crore rupees",
+    Story:"The son of a wealthy, powerful industrialist returns to India and undergoes a remarkable transformation as he becomes consumed by a quest for vengeance against those threatening his father's life."
+  },
+  {
+    Title:"Lagaan",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/b/b6/Lagaan.jpg",
+    Year:2001,
+    Release_Date:"15 June 2001",
+    Duration:"224 minutes",
+    Actors:["Aamir Khan, Gracy Singh, Rachel Shelley, Paul Blackthorne"],
+    Director:"Ashutosh Gowariker",
+    Geners:["History, Action, Sport, Political drama, Comedy, Romance, Adventure"],
+    Languages:"Hindi, Engilsh",
+    Box_office_collection:"659.7 million rupees",
+    Story:"The year is 1893 and India is under British occupation. In a small village, the tyrannical Captain Russell (Paul Blackthorne) has imposed an unprecedented land tax on its citizens. Outraged, Bhuvan (Aamir Khan), a rebellious farmer, rallies the villagers to publicly oppose the tax."
+  },
+  {
+    Title:"The Kerala Story",
+    Poster:"https://upload.wikimedia.org/wikipedia/en/d/d0/The_Kerala_Story_poster.jpg",
+    Year:2023,
+    Release_Date:"5 May 2023",
+    Duration:"138 minutes",
+    Actors:["Adah Sharma, Yogita Bihani, Sonia Balani, Siddhi Idnani"],
+    Director:"Sudipto Sen",
+    Geners:["Crime film, Drama"],
+    Languages:"Hindi",
+    Box_office_collection:"303.97 crore rupees",
+    Story:"'The Kerala Story' narrates the tales of three young girls from various regions of Kerala, with the primary focus on the story of Shalini, who is abducted and subsequently converted to Islam. Shalini is then radicalized. And compelled to join ISIS as a terrorist."
   }
 ]
 const mainDiv = document.getElementById("movies")
 function displayData (data)
 {
-    data.forEach (function (ele)
+    data.forEach(function (ele)
     {
-       const someDiv = document.createElement("div") // This creates div element <div></div>
-       someDiv.classList.add("movies") // This creates class for the div element i.e <div class="movies"></div>
-       const heading = document.createElement('h1') // This creates h1 element i.e <h1></h1>
-       heading.textContent = ele.Title
-       const image = document.createElement('img')
+		const someDiv = document.createElement("div") //<div></div>
+        someDiv.classList.add("movies") //<div class="cartoon"></div>
+        const heading = document.createElement('h2')//<h1></h1>
+        heading.textContent = ele.Title
+        const image = document.createElement('img')
         image.setAttribute('src', ele.Poster)
-        const desc = document.createElement('p')
-        desc.textContent="Released:"+ ele.Year
+        const year = document.createElement('h3')
+        year.textContent="Released:"+ ele.Year
         image.alt=ele.Title
-        someDiv.append(heading,image,desc)
-        mainDiv.appendChild(someDiv)
-    })
+        const desc = document.createElement("p")
+        desc.textContent = ele.Story
+        someDiv.append(heading,image,year,desc)
+        mainDiv.appendChild(someDiv)//<div id="library">
+        //<div class="cartoon"</div>
+        // </div >
+	})
 }
 displayData(data)
